069b9fadd2567b35849ef75cd03f9b31e9c9de82
geraldok7 <geraldoaugustodf@gmail.com>
2025-01-22 14:54:11 -0300

    docker2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Escuta em todas as interfaces de rede
+    port: 5173,        // Porta que o Vite usará (garanta que esteja configurada também no Docker)
+    strictPort: true,  // Força o uso da porta especificada
+  },
 })
